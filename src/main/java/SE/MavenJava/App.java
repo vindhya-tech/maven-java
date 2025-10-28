5107465cf325b1618075b895d2b4497680fb33ec
sharanya2504 <159692454+sharanya2504@users.noreply.github.com>
2025-10-28 12:15:44 +0530

    Change output message from 'Hello World!' to 'Hello!'

--- src/main/java/SE/MavenJava/App.java
+++ src/main/java/SE/MavenJava/App.java
@@ -5,6 +5,6 @@ package SE.MavenJava;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello!");
     }
 }
